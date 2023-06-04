32d4c72a1917fb31450d7687f0c4682272fb400b
VineetTambe <tambevineet@gmail.com>
2023-06-04 19:29:16 -0400

    feat: added debugging code to store the intermediate map to a local file

--- src/multi_robot_planner.cpp
+++ src/multi_robot_planner.cpp
@@ -1,8 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include <nav_msgs/msg/occupancy_grid.hpp>
 #include <nav_msgs/srv/get_map.hpp>
-
-// #include <vector>
+#include <fstream>
 
 void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg)
 {
@@ -10,29 +9,46 @@ void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg)
   std::cout << "Received map with width=" << msg->info.width << " and height=" << msg->info.height << std::endl;
 }
 
-void parseMap(std::vector<std::vector<char>> & occupancy_map, const nav_msgs::msg::OccupancyGrid& map){
-      // for i in range(row_num):
-      //   tmp_str = ""
-      //   for j in range(col_num):
-      //       if mat[i, j] == 1:
-      //           tmp_str += "@"
-      //       else:
-      //           tmp_str += "."
-      //   tmp_str += "\n"
-      //   f.write(tmp_str)
+// debug function
+void writeMapToFile(std::vector<std::vector<char>> & occupancy_map, const std::string& file_path)
+{
+  std::ofstream file(file_path);
+  if (!file.is_open()) {
+    RCLCPP_ERROR(rclcpp::get_logger("service_client"), "Failed to open file for writing");
+    return;
+  }
 
-    for (int i = 0; i < (int)map.info.height; i++) {
-      for (int j = 0; j < (int)map.info.width; j++) {
-        if (map.data[i * (int)map.info.width + j] > 50) 
-        { 
-          occupancy_map[i][j] = '@';
-        } 
-        else 
-        {
-          occupancy_map[i][j] = '.';
-        }
+  // Write map data to file
+  for (int i = 0; i < occupancy_map.size(); ++i) {
+    for (int j = 0; j < occupancy_map[0].size(); ++j) {
+      // int index = i * map.info.width + j;
+      file << static_cast<int>(occupancy_map[i][j]) << " ";
+    }
+    file << std::endl;
+  }
+
+  file.close();
+
+  RCLCPP_INFO(rclcpp::get_logger("service_client"), "Map written to file: %s", file_path.c_str());
+}
+
+
+void parseMap(std::vector<std::vector<char>> & occupancy_map, const nav_msgs::msg::OccupancyGrid& map){
+  for (int i = 0; i < (int)map.info.height; i++) {
+    for (int j = 0; j < (int)map.info.width; j++) {
+      if (map.data[i * (int)map.info.width + j] > 50) 
+      { 
+        occupancy_map[i][j] = '@';
+      } 
+      else 
+      {
+        occupancy_map[i][j] = '.';
       }
     }
+  }
+
+  writeMapToFile(occupancy_map, "/home/admin/ddg_mfi/mp_400_ws/svd_demo-parsed-map.txt");
+
 }
 
 void printMap(std::vector<std::vector<char>>& map) 
@@ -71,50 +87,13 @@ int main(int argc, char* argv[])
   auto node = std::make_shared<rclcpp::Node>("multi_robot_planner_node");
 
   // Create the subscriber to the "/map" topic
-  auto subscriber = node->create_subscription<nav_msgs::msg::OccupancyGrid>("/map",
-                                                                            10,  // QoS history depth
-                                                                            mapCallback);
-  
-
-  // auto client = node->create_client<nav_msgs::srv::GetMap>("/map_server/map");
-
-  // // Wait for the service to be available
-  // while (!client->wait_for_service(std::chrono::seconds(1))) {
-  //   if (!rclcpp::ok()) {
-  //     RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service");
-  //     return 1;
-  //   }
-  //   RCLCPP_INFO(node->get_logger(), "Service not available, waiting...");
-  // }
-
-  // RCLCPP_INFO(node->get_logger(), "Service found!");
-
-  // auto request = std::make_shared<nav_msgs::srv::GetMap::Request>();
-
-  // // Send the service request
-  // auto result = client->async_send_request(request);
-
-  // try {
-  //   // Check if the future has a valid response
-  //   if (result.valid() && result.wait_for(std::chrono::seconds(15)) == std::future_status::ready) {
-  //     // auto response = result.get();
-
-  //     // // Access the map data
-  //     // nav_msgs::msg::OccupancyGrid map = response->map;
-  //     occupancy_map.resize(result.get()->map.info.height, std::vector<char>(result.get()->map.info.width, 0));
-  //     parseMap(occupancy_map, result.get()->map);
-
-  //     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Successfully received map");
-  //     printMap(occupancy_map);
-  //     // Perform any cleanup or necessary actions
-  //   } else {
-  //     throw std::runtime_error("Service response not received");
-  //   }
-  // } catch (const std::exception& e) {
-  //   RCLCPP_ERROR(node->get_logger(), "Service call failed: %s", e.what());
-  // }
+  // auto subscriber = node->create_subscription<nav_msgs::msg::OccupancyGrid>("/map",
+  //                                                                           10,  // QoS history depth
+  //                                                                           mapCallback);
 
 
+  // service client to subscriber to the static map
+  // TODO update this to consider dynamics obstacles as well.
   auto client = node->create_client<nav_msgs::srv::GetMap>("/map_server/map");
 
   // Wait for the service to be available
@@ -131,13 +110,7 @@ int main(int argc, char* argv[])
   // Send the service request
   auto future = client->async_send_request(request);
 
-  // Wait for the service response and process it asynchronously
-  // Wait for the service response and process it asynchronously
-  // auto result_callback = std::bind(handle_response, std::placeholders::_1);
-  // auto callback_group = node->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
-  // rclcpp::spin_until_future_complete(node, future, callback_group, result_callback);
-
-    // Create a callback to handle the response
+  // Create a callback to handle the response
   auto handle_response_callback = [&node](rclcpp::Client<nav_msgs::srv::GetMap>::SharedFuture future) {
     try {
       auto response = future.get();
@@ -151,28 +124,23 @@ int main(int argc, char* argv[])
   // Spin using spin_once() in a while loop until the response is received
   while (rclcpp::ok()) {
     rclcpp::spin_some(node);
+    // map service subscriber
     if (future.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
       handle_response_callback(future);
-      break;
     }
   }
 
+  // crete a service call to get paths for n robots -> service call.
+  /** Service call accepts the following arguments:
+  1. start position of the robot
+  2. goal postion of the robot
+  3. robot namespaces
 
-
-  // TODO create an instance of MultiSpaceTimeAstarPlanner class
-
-  // TODO initalize the graph of the planner with the map
-
-  // TODO create a service call exposing the MultiSpaceTimeAstarPlanner objects plan function
-
-  // Spin the node so the subscriber can receive messages
-  // rclcpp::spin(node);
-
-  // rclcpp::WallRate loop_rate(30);
-  // while (rclcpp::ok()) {
-  //     rclcpp::spin_some(node);
-  //     loop_rate.sleep();
-  // }
+  Service call returns the following:
+  1. A hashmap of posestamped messages with pose for each robot.
+  */  
+  
+  // TODO call this service call from multi-navigator node.
 
   // Shutdown ROS2
   rclcpp::shutdown();
