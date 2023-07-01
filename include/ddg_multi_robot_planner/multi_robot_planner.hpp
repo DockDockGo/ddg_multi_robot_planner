ff11c7b7e2a31dbe92fbb6cd38e927ae7fcc1b87
VineetTambe <tambevineet@gmail.com>
2023-07-01 19:02:22 -0400

    integrating cbs solver v0

--- include/ddg_multi_robot_planner/multi_robot_planner.hpp
+++ include/ddg_multi_robot_planner/multi_robot_planner.hpp
@@ -2,21 +2,23 @@
 #define MULTI_ROBOT_PLANNER_H
 
 #include "rclcpp/rclcpp.hpp"
-#include <fstream>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <nav_msgs/msg/occupancy_grid.hpp>
 #include <nav_msgs/srv/get_map.hpp>
-#include <string>
 #include <tf2/exceptions.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-#include <vector>
 // TODO(@VineetTambe: replace unordered_map with boost:flatmap for efficiency)
 #include "ddg_multi_robot_srvs/srv/get_multi_plan.hpp"
 #include <unordered_map>
 // #include <boost/program_options.hpp>
 // #include <boost/tokenizer.hpp>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
 #include <CBS.h>
 
 namespace multi_robot_planner
@@ -88,6 +90,9 @@ namespace multi_robot_planner
         std::string _agent_scen_file_name =
             "/home/admin/ddg_mfi/mp_400_ws/src/ddg_multi_robot_planner/maps/"
             "svd_demo-scen.txt";
+        std::string _planned_path_file_name =
+            "/home/admin/ddg_mfi/mp_400_ws/src/ddg_multi_robot_planner/maps/"
+            "planned-paths.txt";
         int _map_height;
         int _map_width;
         bool createAgentScenarioFile(
@@ -95,6 +100,35 @@ namespace multi_robot_planner
             std::vector<geometry_msgs::msg::Pose> &robot_goal_poses,
             std::string &map_file_name, int &map_height, int &map_width,
             std::string &file_path);
+
+        bool readPlannedPathFromFile(const std::string &filename,
+                                     std::vector<nav_msgs::msg::Path> &planned_paths);
+
+        // CBS object
+        // CBS cbs;
+        int _agentNum = 5;          // number of agents | default 5
+        std::string _agentIdx = ""; // customize the indices of the agents (e.g., \"0,1\")
+        int _rows = 0;              // number of rows | default 0
+        int _cols = 0;              // number of cols | default 0
+        int _num_obstacles = 0;     // number of obstacles | default 0
+        int _warehouseWidth = 0;    // width of working stations on both sides, for generating instances | default 0
+        int _max_runs = 1;
+        bool _use_sipp = false;            // using sipp as the single agent solver | default false
+        int _display_output_on_screen = 0; // screen option (0: none; 1: results; 2:all)
+
+        bool _prioritizingConflicts = true; // conflict priortization. If true, conflictSelection is used as a tie-breaking rule.
+        bool _disjointSplitting = false;    // disjoint splitting. If true, disjoint splitting is used.
+        bool _bypass = true;
+        heuristics_type _heuristics = heuristics_type::WDG;              // heuristics for the high-level search (Zero, CG,DG, WDG)| default WDG
+        rectangle_strategy _rectangleReasoning = rectangle_strategy::GR; // generalized rectangle reasoning
+        corridor_strategy _corridorReasoning = corridor_strategy::GC;
+        bool _targetReasoning = true;
+        bool _mutexReasoning = false;
+        bool _saving_stats = false;
+        int _nodeLimit = MAX_NODES;
+
+        double _cutoffTime = 60.0; // cutoff time in seconds | default 60.0
+        void configure();
     };
 
 } // namespace multi_robot_planner
